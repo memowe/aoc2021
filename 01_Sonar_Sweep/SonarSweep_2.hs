7bc440dbc45680ce3af672cfbc369f20365d9021
Mirko Westermeier <mirko@westermeier.de>
2021-12-01 11:53:51 +0100

    Add second and last part of Day 1

---
+++ 01_Sonar_Sweep/SonarSweep_2.hs
@@ -0,0 +1,10 @@
+module SonarSweep_2 where
+
+import Data.List
+
+main = interact (show . sonarSweep . map read . lines)
+  where sonarSweep :: [Int] -> Int
+        sonarSweep  = length . filter id . goingUp . map sum . window 3
+        goingUp     = (False :) . (zipWith (<) <*> tail)
+        window size = filter ((== size) . length) . map (take size) . tails
+
