ba8ddcb4726b2ef0c808cc99157b7ab565c49d06
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2021-12-02 08:15:23 +0100

    Add solution to day 2 part 1

---
+++ 02_Dive/Dive_1.hs
@@ -0,0 +1,12 @@
+module Dive_1 where
+
+parse :: String -> (Int, Int)
+parse s = let [dir,a] = words s; amount = read a
+          in  case dir of "forward" -> (amount, 0)
+                          "up"      -> (0, negate amount)
+                          "down"    -> (0, amount)
+
+main = interact (show . prod . dive . map parse . lines)
+  where dive                = foldl goto (0,0)
+        prod (x,y)          = x * y
+        goto (x,y) (px,py)  = (px + x, py + y)
