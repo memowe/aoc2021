81f6e8cec08484d0fe9c524056abc710d4a95bbd
Mirko Westermeier <mirko@westermeier.de>
2021-12-04 12:23:38 +0100

    4.1: Add board types and simple value selection

---
+++ 04_Giant_Squid/GiantSquid_1.hs
@@ -0,0 +1,33 @@
+import Data.Either
+import Data.List
+import Data.List.Split
+import Data.Function
+
+newtype Position a = Position { value :: Either a a }
+newtype Board a    = Board    { rows :: [[Position a]] }
+
+instance Show a => Show (Position a) where
+  show (Position (Left p))  = " " ++ show p ++ " "
+  show (Position (Right s)) = "[" ++ show s ++ "]"
+
+instance Show a => Show (Board a) where
+  show = unlines . map (unwords . (show <$>)) . rows
+
+cols :: Board a -> [[Position a]]
+cols = transpose . rows
+
+createBoard :: [[a]] -> Board a
+createBoard = Board . map (Position . Left <$>)
+
+hasWon :: Board a -> Bool
+hasWon = ((||) `on` any rights) <$> rows <*> cols
+  where rights = all (isRight . value)
+
+selectPos :: Eq a => a -> Position a -> Position a
+selectPos val p@(Position (Left v))
+  | v == val  = Position (Right v)
+  | otherwise = p
+selectPos _ p = p
+
+select :: Eq a => a -> Board a -> Board a
+select val = Board . map (map (selectPos val)) . rows
