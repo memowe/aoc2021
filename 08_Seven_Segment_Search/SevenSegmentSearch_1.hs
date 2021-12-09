10ca598c1baeea418182b97138ea7cb684db88a1
Mirko Westermeier <mirko@westermeier.de>
2021-12-09 12:57:04 +0100

    8.1: add solution

--- 08_Seven_Segment_Search/SevenSegmentSearch_1.hs
+++ 08_Seven_Segment_Search/SevenSegmentSearch_1.hs
@@ -1,7 +1,7 @@
 import Data.List.Split
 import Data.Function
 
-parse :: String -> ([String], [String])
-parse = (((,) `on` words . head) <*> tail) . splitOn " | "
-
-main = interact $ show . parse
+main = interact $ show . countUnique . parse
+  where parse       = concatMap parseLine . lines
+        parseLine   = words . (!! 1) . splitOn " | "
+        countUnique = length . filter ((`elem` [2,4,3,7]) . length)
